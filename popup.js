e40ade95d23015081222586152910ed33313be11
RikeshThapa <rthapa@u.rochester.edu>
2015-09-21 22:24:28 -0700

    Adding chrome.storage api calls for storage

--- popup.js
+++ popup.js
@@ -1,5 +1,23 @@
 $(function(){
-	$('#name').keyup(function(){
-		$('#greeting').text('Hi, ' + $('#name').val() + '!');
+	chrome.storage.sync.get('total', function(items){
+		$('#total').text(items.total);
+	});
+	
+	$("#addAmount").click(function(){
+		chrome.storage.sync.get('total', function(items){
+			var newTotal = 0;
+			if(items.total){
+				newTotal += parseInt(items.total);
+			}
+			
+			var amount = $('#amount').val();
+			if(amount){
+				newTotal += parseInt(amount);
+			}
+			
+			chrome.storage.sync.set({'total' : newTotal });
+			$('#total').text(newTotal);
+			$('#amount').val('');
+		});
 	});
 });
